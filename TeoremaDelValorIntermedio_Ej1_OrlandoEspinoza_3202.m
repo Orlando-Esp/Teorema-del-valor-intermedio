b221b64daa9da86227345a86cd673da6413921ab
Orlando-Esp <82452841+Orlando-Esp@users.noreply.github.com>
2021-05-27 12:27:39 -0500

    Create TeoremaDelValorIntermedio_Ej1_OrlandoEspinoza_3202.m

---
+++ TeoremaDelValorIntermedio_Ej1_OrlandoEspinoza_3202.m
@@ -0,0 +1,24 @@
+% Octave Script
+% Title			:Teorema Del Valor Intermedio ej1.
+% Description		:Script para desarrrollar el ejericio1 de la Teorema Del Valor Intermedioa.
+% Author		:Orlando Espinoza Valentin (Orlando-Esp) espinozaorlando978@gmail.com
+% Date			:20210527
+% sion		        :1
+% Usage			:octave> /path/Teorema Del Valor Intermedio,TeoremaDelValorIntermedio_Ej1_OrlandoEspinoza_3202.m
+% Notes			:Requiere aplicacion octave-online usar en consola preferentemente. 
+%program execution page :https://octave-online.net
+
+%Se encarga de limpiar variables y pantalla
+clc, clear  
+%funcion definida 
+x=-2:.05:3;
+f=inline('3*x-2');
+plot(x,f(x),'r','LineWidth',2)
+valor_de_Integral=quad(f,-2,3)
+Valor_Medio=strcat('3*x-2==',num2str(1/3*valor_de_Integral))
+%Deduccion geometrica
+hold on
+area(x,f(x))
+altura=f(c(1));
+plot([-2 3],[altura altura])
+hold off
